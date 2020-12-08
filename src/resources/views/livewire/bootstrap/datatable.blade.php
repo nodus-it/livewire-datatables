bc3d11664d3fb44005226a2512174af322199212
Dominique Heinelt <dominique.heinelt@nano-consult.de>
2020-12-08 11:25:56 +0100

    Update datatable.blade.php
    Some responsive optimizations for the datatable footer section

--- src/resources/views/livewire/bootstrap/datatable.blade.php
+++ src/resources/views/livewire/bootstrap/datatable.blade.php
@@ -13,15 +13,15 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-4">
+        <div class="col-lg-4 order-lg-1 col-7">
             @include('nodus.packages.livewire-datatables::livewire.' . config('livewire-datatables.theme') . '.components.widgets.counter')
         </div>
-        <div class="col-4 text-center">
+        <div class="col-lg-4 col-12 order-first order-lg-2 text-center">
             <div class="nodus-table-pagination d-inline-block">
                 {{$results->links()}}
             </div>
         </div>
-        <div class="col-4 text-right">
+        <div class="col-lg-4 col-5 order-last text-right">
             @include('nodus.packages.livewire-datatables::livewire.' . config('livewire-datatables.theme') . '.components.widgets.page_length')
         </div>
     </div>
