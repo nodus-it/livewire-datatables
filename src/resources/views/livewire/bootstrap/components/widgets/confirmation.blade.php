53fb9e9ad92272b14b5f6ceef173b5764d9caa04
Dominique Heinelt <dominique.heinelt@nano-consult.de>
2021-01-21 16:44:04 +0100

    Removed some unnecessary stylesheets and switched to an generic times icon for the confirmation modal

--- src/resources/views/livewire/bootstrap/components/widgets/confirmation.blade.php
+++ src/resources/views/livewire/bootstrap/components/widgets/confirmation.blade.php
@@ -1,11 +1,7 @@
 <div>
     <style>
-        body {
-            font-family: 'Varela Round', sans-serif;
-        }
-
         .modal-confirm {
-            color: #636363;
+            color: var(--gray);
             width: 400px;
         }
 
@@ -34,20 +30,9 @@
             right: -2px;
         }
 
-        .modal-confirm .modal-body {
-            color: #999;
-        }
-
         .modal-confirm .modal-footer {
             border: none;
-            text-align: center;
-            border-radius: 5px;
-            font-size: 13px;
-            padding: 10px 15px 25px;
-        }
-
-        .modal-confirm .modal-footer a {
-            color: #999;
+            padding-top: 0;
         }
 
         .modal-confirm .icon-box {
@@ -55,46 +40,15 @@
             height: 80px;
             margin: 0 auto;
             border-radius: 50%;
-            z-index: 9;
             text-align: center;
-            border: 3px solid #f15e5e;
+            border: 3px solid var(--danger);
         }
 
-        .modal-confirm .icon-box i {
-            color: #f15e5e;
+        .modal-confirm .icon-box span {
+            color: var(--danger);
             font-size: 46px;
             display: inline-block;
-            margin-top: 13px;
-        }
-
-        .modal-confirm .btn, .modal-confirm .btn:active {
-            color: #fff;
-            border-radius: 4px;
-            background: #60c7c1;
-            text-decoration: none;
-            transition: all 0.4s;
-            line-height: normal;
-            min-width: 120px;
-            border: none;
-            min-height: 40px;
-            border-radius: 3px;
-            margin: 0 5px;
-        }
-
-        .modal-confirm .btn-secondary {
-            background: #c1c1c1;
-        }
-
-        .modal-confirm .btn-secondary:hover, .modal-confirm .btn-secondary:focus {
-            background: #a8a8a8;
-        }
-
-        .modal-confirm .btn-danger {
-            background: #f15e5e;
-        }
-
-        .modal-confirm .btn-danger:hover, .modal-confirm .btn-danger:focus {
-            background: #ee3535;
+            margin-top: 2px;
         }
     </style>
 
@@ -103,7 +57,7 @@
             <div class="modal-content">
                 <div class="modal-header flex-column">
                     <div class="icon-box">
-                        <i class="material-icons">&#xE5CD;</i>
+                        <span>✕</span>
                     </div>
                     <h4 class="modal-title w-100">{{$confirmation['title'] ?? trans('nodus.packages.livewire-datatables::datatable.confirm.title')}}</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
