d390434b2569c5c2a02d2c29572454ade6fbb036
Dominique Heinelt <dominique.heinelt@immo-finanzcheck.de>
2024-03-22 16:46:01 +0100

    Fixed wire:model occurrences

--- src/resources/views/livewire/bootstrap/components/widgets/search.blade.php
+++ src/resources/views/livewire/bootstrap/components/widgets/search.blade.php
@@ -1,6 +1,6 @@
 @if($show->search === true)
 <div class="nodus-table-search">
-    <input wire:model.debounce.300ms="search"
+    <input wire:model.live.debounce.300ms="search"
            class="form-control form-control-sm ml-auto"
            placeholder="@lang('nodus.packages.livewire-datatables::datatable.search.placeholder')"
            type="text">
